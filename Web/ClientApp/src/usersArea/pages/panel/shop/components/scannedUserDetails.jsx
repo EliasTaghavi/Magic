71b7c95e652a5fd63eabefee1e4013c19f21738b
shahabt24 <shahab_t24@yahoo.com>
2022-01-07 21:29:52 +0330

    scanned user

---
+++ Web/ClientApp/src/usersArea/pages/panel/shop/components/scannedUserDetails.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Modal} from "react-bootstrap";
+import Loader from "react-loader-spinner";
+
+const ScannedUserDetails = ({data, onClose}) => {
+	let tt = '1400/01/01';
+	return (
+		<Modal
+			size="md"
+			centered={true}
+			show={true}
+			onHide={onClose}>
+			<div className="modal-header w-100 d-flex align-items-center justify-content-start fs22 text-dark">
+				وضعیت حساب مشتری
+			</div>
+			<div className="modal-body py-5 transition">
+				{data && <div className="d-flex centered">
+					<div className="packageContainerNoHover shadow w-100 pckBorder">
+						<p className="fs34 textSecondary1 m-0">شهاب طالبی</p>
+						<p className="fs26 textThird m-0 mt-3">دانشجو</p>
+						<hr className="w-100 cDivider"/>
+						<p className="fs60 m-0 text-success text-center cNumber mt-1">معتبر</p>
+						<hr className="w-100 cDivider"/>
+						<p className="fs14 textThird m-0 mt-1">مدت اعتبار باقی مانده: <span
+							className="textMain fs20 font-weight-bold">2</span> روز</p>
+						<p className="fs14 textThird m-0 mt-1">{`تاریخ انقضا:\xa0${tt}`}</p>
+					</div>
+				</div>}
+				{data && <div className="d-flex centered mt-3">
+					<button type="button" className="btn border-0 submitBtn" style={{maxWidth: 300}}>
+						ثبت خرید
+					</button>
+				</div>}
+				{!data && (
+					<div className="w-100 d-flex centered">
+						<Loader type="ThreeDots" color='#ff521d' height={10} width={70} className="loader"/>
+					</div>
+				)}
+			</div>
+		</Modal>
+	);
+}
+
+export default ScannedUserDetails;
--- Web/ClientApp/src/usersArea/pages/panel/shop/shopDashboard.component.jsx
+++ Web/ClientApp/src/usersArea/pages/panel/shop/shopDashboard.component.jsx
@@ -1,6 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useHistory} from "react-router-dom";
+import {getUserDetailsInShop} from "../../../api/shop/scannedUser";
+import {toast} from "react-toastify";
+import toastOptions from "../../../../components/ToastOptions";
+import ScannedUserDetails from "./components/scannedUserDetails";
 
 const ShopDashboard = () => {
+	const history = useHistory();
+	const [scannedUserDetailsModal, setScannedUserDetailsModal] = useState(false);
+	const [currentPck, setCurrentPck] = useState(null);
+
+	useEffect(() => {
+		let userId = history?.location?.pathname?.replace('/shop-panel/', '');
+		if (userId.length > 0) {
+			setScannedUserDetailsModal(true);
+			getUserDetails(userId);
+		}
+	}, []);
+
+	const getUserDetails = (userId) => {
+		getUserDetailsInShop(userId)
+			.then((response) => {
+				if (response) {
+					let {success, result} = response
+					if (response === 401) {
+						// do nothing but in another api's should logout from system
+					} else if (success) {
+						setCurrentPck(result);
+					}
+				} else {
+					toast.error('خطای سرور', toastOptions);
+				}
+			})
+			.catch((error) => {
+				toast.error('خطای سرور', toastOptions);
+			})
+	};
+
 	return (
 		<div className="card cardPrimary px-3">
 			<div className="card-header bg-transparent">
@@ -9,6 +45,7 @@ const ShopDashboard = () => {
 			<div className="w-100 d-flex align-items-start justify-content-start py-5 px-3">
 
 			</div>
+			{scannedUserDetailsModal && <ScannedUserDetails data={currentPck} onClose={() => setScannedUserDetailsModal(false)} />}
 		</div>
 	);
 }
