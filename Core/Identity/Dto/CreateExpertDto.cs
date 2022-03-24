a3ce703c17823c8148c47ae61c2d2046b0394021
Elias Taghavi <elias.taghavi.rz@outlook.com>
2022-03-24 17:11:52 +0430

    CreateExpert

---
+++ Core/Identity/Dto/CreateExpertDto.cs
@@ -0,0 +1,15 @@
+﻿using Core.Identity.Entities;
+
+namespace Core.Identity.Dto
+{
+    public class CreateExpertDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Mobile { get; set; }
+        public string Address { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Password { get; set; }
+        public string UserTypeId { get; set; }
+    }
+}
